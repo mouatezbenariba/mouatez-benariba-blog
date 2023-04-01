a24ea0ad84f64b0b8ec4e236b8c491e5e125f3e2
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-04-01 23:15:43 +0100

    add prismjs to eleventy config and prism node files

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require('luxon');
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/css/style.css');
@@ -8,6 +9,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
