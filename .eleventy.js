ccfa7d6b8504eeab063b0a8270aee924b410f311
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-04-02 15:57:13 +0100

    remove options object from eleventy config file

--- .eleventy.js
+++ .eleventy.js
@@ -10,30 +10,7 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-  eleventyConfig.addPlugin(syntaxHighlight, {
-
-    // Change which Eleventy template formats use syntax highlighters
-    templateFormats: ["*"], // default
-
-    // Use only a subset of template types (11ty.js added in v4.0.0)
-    // templateFormats: ["liquid", "njk", "md", "11ty.js"],
-
-    // init callback lets you customize Prism
-    init: function({ Prism }) {
-      Prism.languages.myCustomLanguage = /* */;
-    },
-
-    // Added in 3.1.1, add HTML attributes to the <pre> or <code> tags
-    preAttributes: {
-      tabindex: 0,
-
-      // Added in 4.1.0 you can use callback functions too
-      "data-language": function({ language, content, options }) {
-        return language;
-      }
-    },
-    codeAttributes: {},
-  });
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   // create an array of unique tags from your blog posts
   eleventyConfig.addCollection('tagList', function (collection) {
