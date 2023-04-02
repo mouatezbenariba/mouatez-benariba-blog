bbe285e936a8a67b808a2f79451b13f09fa89a41
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-04-02 15:44:26 +0100

    edit prism function

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,30 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(syntaxHighlight, {
+
+    // Change which Eleventy template formats use syntax highlighters
+    templateFormats: ["*"], // default
+
+    // Use only a subset of template types (11ty.js added in v4.0.0)
+    // templateFormats: ["liquid", "njk", "md", "11ty.js"],
+
+    // init callback lets you customize Prism
+    init: function({ Prism }) {
+      Prism.languages.myCustomLanguage = /* */;
+    },
+
+    // Added in 3.1.1, add HTML attributes to the <pre> or <code> tags
+    preAttributes: {
+      tabindex: 0,
+
+      // Added in 4.1.0 you can use callback functions too
+      "data-language": function({ language, content, options }) {
+        return language;
+      }
+    },
+    codeAttributes: {},
+  });
 
   // create an array of unique tags from your blog posts
   eleventyConfig.addCollection('tagList', function (collection) {
