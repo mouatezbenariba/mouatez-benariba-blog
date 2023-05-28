cd35807a027bee5c25deab380431bea3bc30be39
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-05-28 14:03:21 +0100

    update blog style

--- .eleventy.js
+++ .eleventy.js
@@ -29,7 +29,30 @@ module.exports = function (eleventyConfig) {
     });
   });
 
-  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    // Line separator for line breaks
+    lineSeparator: '\n',
+
+    // Change which Eleventy template formats use syntax highlighters
+    templateFormats: ['*'], // default
+
+    // Use only a subset of template types (11ty.js added in v4.0.0)
+    // templateFormats: ["liquid", "njk", "md", "11ty.js"],
+
+    // Added in 3.1.1, add HTML attributes to the <pre> or <code> tags
+    preAttributes: {
+      tabindex: 0,
+
+      // Added in 4.1.0 you can use callback functions too
+      'data-language': function ({ language, content, options }) {
+        return language;
+      },
+    },
+    codeAttributes: {},
+
+    // Added in 5.0.0, throw errors on invalid language names
+    errorOnInvalidLanguage: false,
+  });
 
   function filterTagList(tags) {
     return (tags || []).filter(
